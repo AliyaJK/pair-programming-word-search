8367277f4d0ce25132e1c48c11601244c50ea97b
AliyaJK <kanani.aliya@gmail.com>
2023-04-24 01:04:59 +0000

    update function to return false when word not found and to search vertically

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,23 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map(ls => ls.join(''));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  };
 
-module.exports = wordSearch
+  // check vertically
+  for (let i = 0; i < letters[0].length; i++) {
+    let column = '';
+    for (let j = 0; j < letters.length; j++) {
+      column += letters[j][i];
+    }
+    if (column.includes(word)) {
+      return true;
+    }
+  }
+
+  // word not found
+  return false;
+};
+
+
+module.exports = wordSearch;
